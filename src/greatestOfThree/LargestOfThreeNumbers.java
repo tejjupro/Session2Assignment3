c463fad2027cdbc4cbfad30234ad1b32c60d81d7
bhusan ripan <bhusan ripan@DESKTOP-2JJBOHV>
2017-06-13 14:37:38 +0530

    comments

--- src/greatestOfThree/LargestOfThreeNumbers.java
+++ src/greatestOfThree/LargestOfThreeNumbers.java
@@ -2,19 +2,22 @@
 package greatestOfThree;
 import java.util.Scanner;
 
-public class LargestOfThreeNumbers {
+public class LargestOfThreeNumbers
+// main class
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) //main method
+	{
 
 	      int x, y, z;
 	      System.out.println("Enter three integers ");
-	      Scanner in = new Scanner(System.in);
+	      Scanner in = new Scanner(System.in); //scanner to get input from user on output
 	 
 	      x = in.nextInt();
 	      y = in.nextInt();
 	      z = in.nextInt();
 	 
-	      if ( x > y && x > z )
+	      if ( x > y && x > z )  // if else loop used to satisfy condition
 	         System.out.println("The Largest number is :"+x);
 	      else if ( y > x && y > z )
 	         System.out.println("The Largest number is :" +y);
@@ -23,6 +26,6 @@ public class LargestOfThreeNumbers {
 	      else   
 	         System.out.println("Entered numbers are not distinct.");
 
-	}
+	} //class ends here
 
-}
+} //main method ends here
